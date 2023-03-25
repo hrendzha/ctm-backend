c95ee1e0a2fae82807298706d43cd771f0865328
hrendzha <pashahrendzha99@gmail.com>
2023-03-26 01:06:44 +0200

    added two DB env for development and production

--- src/infra/Database.ts
+++ src/infra/Database.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
 class Database {
-  private DB_HOST = process.env.DB_HOST || "";
+  private readonly DB_HOST =
+    process.env.NODE_ENV === "production" ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV;
 
   async init() {
-    const { DB_HOST } = this;
+    const { DB_HOST = "" } = this;
 
     try {
       await mongoose.connect(DB_HOST);
       console.log("Database connection successful");
     } catch (error) {
+      console.log(error);
       throw new Error("Database connection error");
     }
   }
